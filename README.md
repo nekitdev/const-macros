650d446d5a45fb4b86a0e636a051f1a63996c89f
Nikita Tikhonov <nekitdevofficial@gmail.com>
2025-02-12 22:38:04 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,78 @@
+# `const-macros`
+
+[![License][License Badge]][License]
+[![Version][Version Badge]][Crate]
+[![Downloads][Downloads Badge]][Crate]
+[![Test][Test Badge]][Actions]
+
+> *Various macros for const contexts.*
+
+## Installation
+
+### `cargo`
+
+You can add `const-macros` as a dependency with the following command:
+
+```console
+$ cargo add const-macros
+```
+
+Or by directly specifying it in the configuration like so:
+
+```toml
+[dependencies]
+const-macros = "0.1.0"
+```
+
+Alternatively, you can add it directly from the source:
+
+```toml
+[dependencies.const-macros]
+git = "https://github.com/nekitdev/const-macros.git"
+```
+
+## Documentation
+
+You can find the documentation [here][Documentation].
+
+## Support
+
+If you need support with the library, you can send an [email][Email].
+
+## Changelog
+
+You can find the changelog [here][Changelog].
+
+## Security Policy
+
+You can find the Security Policy of `const-macros` [here][Security].
+
+## Contributing
+
+If you are interested in contributing to `const-macros`, make sure to take a look at the
+[Contributing Guide][Contributing Guide], as well as the [Code of Conduct][Code of Conduct].
+
+## License
+
+`const-macros` is licensed under the MIT License terms. See [License][License] for details.
+
+[Email]: mailto:support@nekit.dev
+
+[Discord]: https://nekit.dev/chat
+
+[Actions]: https://github.com/nekitdev/const-macros/actions
+
+[Changelog]: https://github.com/nekitdev/const-macros/blob/main/CHANGELOG.md
+[Code of Conduct]: https://github.com/nekitdev/const-macros/blob/main/CODE_OF_CONDUCT.md
+[Contributing Guide]: https://github.com/nekitdev/const-macros/blob/main/CONTRIBUTING.md
+[Security]: https://github.com/nekitdev/const-macros/blob/main/SECURITY.md
+
+[License]: https://github.com/nekitdev/const-macros/blob/main/LICENSE
+
+[Crate]: https://crates.io/crates/const-macros
+[Documentation]: https://docs.rs/const-macros
+
+[License Badge]: https://img.shields.io/crates/l/const-macros
+[Version Badge]: https://img.shields.io/crates/v/const-macros
+[Downloads Badge]: https://img.shields.io/crates/dr/const-macros
+[Test Badge]: https://github.com/nekitdev/const-macros/workflows/test/badge.svg
---
+++ src/lib.rs
@@ -0,0 +1,136 @@
+//! Various macros for const contexts.
+//!
+//! # Examples
+//!
+//! Below is an example that utilizes most of the macros provided by this crate.
+//!
+//! ```
+//! use const_macros::{const_assert, const_early, const_ok, const_try};
+//!
+//! use thiserror::Error;
+//!
+//! #[derive(Error, Debug)]
+//! #[error("unexpected length: `{value}`")]
+//! pub struct Error {
+//!     pub value: usize,
+//! }
+//!
+//! impl Error {
+//!     pub const fn new(value: usize) -> Self {
+//!         Self { value }
+//!     }
+//! }
+//!
+//! pub const MIN: usize = 32;
+//! pub const MAX: usize = 96;
+//!
+//! const_assert!(MIN <= MAX);
+//!
+//! #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+//! pub struct Length {
+//!     value: usize,
+//! }
+//!
+//! impl Length {
+//!     pub const fn new(value: usize) -> Result<Self, Error> {
+//!         const_try!(Self::check(value));
+//!
+//!         Ok(unsafe { Self::new_unchecked(value) })
+//!     }
+//!
+//!     pub const fn new_ok(value: usize) -> Option<Self> {
+//!         const_ok!(Self::new(value))
+//!     }
+//!
+//!     pub const fn check(value: usize) -> Result<(), Error> {
+//!         const_early!(value < MIN || value > MAX => Error::new(value));
+//!
+//!         Ok(())
+//!     }
+//!
+//!     pub const unsafe fn new_unchecked(value: usize) -> Self {
+//!         Self { value }
+//!     }
+//!
+//!     pub const MIN: Self = Self::new_ok(MIN).unwrap();
+//!     pub const MAX: Self = Self::new_ok(MAX).unwrap();
+//! }
+//! ```
+
+#![no_std]
+#![deny(missing_docs)]
+
+#[allow(unused_imports)]
+#[doc(hidden)]
+pub mod import {
+    pub use core::{option::Option, result::Result};
+}
+
+/// Similar to the `?` operator but for `const` contexts.
+///
+/// Note that no conversions are performed, as it is impossible in `const` contexts.
+#[macro_export]
+macro_rules! const_try {
+    ($result: expr) => {
+        match $result {
+            $crate::import::Result::Ok(value) => value,
+            $crate::import::Result::Err(error) => return $crate::import::Result::Err(error),
+        }
+    };
+}
+
+/// Same as [`Result::ok`] but for `const` contexts.
+#[macro_export]
+macro_rules! const_ok {
+    ($result: expr) => {
+        match $result {
+            $crate::import::Result::Ok(value) => $crate::import::Option::Some(value),
+            $crate::import::Result::Err(_) => $crate::import::Option::None,
+        }
+    };
+}
+
+/// Same as [`Result::err`] but for `const` contexts.
+#[macro_export]
+macro_rules! const_err {
+    ($result: expr) => {
+        match $result {
+            $crate::import::Result::Ok(_) => $crate::import::Option::None,
+            $crate::import::Result::Err(error) => $crate::import::Option::Some(error),
+        }
+    };
+}
+
+/// Returns early with the provided error if the condition is true.
+#[macro_export]
+macro_rules! const_early {
+    ($condition: expr => $error: expr) => {
+        if $condition {
+            return $crate::import::Result::Err($error);
+        }
+    };
+}
+
+/// Similar to [`assert!`] but for `const` contexts.
+#[macro_export]
+macro_rules! const_assert {
+    ($condition: expr) => {
+        const _: () = assert!($condition);
+    };
+}
+
+/// Similar to [`assert_eq!`] but for `const` contexts.
+#[macro_export]
+macro_rules! const_assert_eq {
+    ($left: expr, $right: expr, $(,)?) => {
+        $crate::const_assert!($left == $right);
+    };
+}
+
+/// Similar to [`assert_ne!`] but for `const` contexts.
+#[macro_export]
+macro_rules! const_assert_ne {
+    ($left: expr, $right: expr, $(,)?) => {
+        $crate::const_assert!($left != $right);
+    };
+}
